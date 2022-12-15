8525e96084487494e3d362cf835342910c0e31b3
Cecilie Dinsen <ceci821s@stud.kea.dk>
2022-12-15 16:40:34 +0100

    cfgcgh

--- src/main.js
+++ src/main.js
@@ -16,20 +16,3 @@ function toggleMenu() {
 }
 
 btn.addEventListener("click", toggleMenu);
-
-// sortiment dropdown
-const knap = document.querySelector("toggle.knap");
-const sortiment = document.querySelector("open_sortiment");
-
-function toggleKnap() {
-  sortiment.classList.toggle("shown");
-
-  const knapShown = sortiment.classList.contains("shown");
-
-  if (knapShown) {
-    knap.textContent = "✕";
-  } else {
-    knap.textContent = "❯";
-  }
-}
-knap.addEventListener("click", knapMenu);
