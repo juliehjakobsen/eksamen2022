ab3b5898640509954504012b9d37deb221a50dc4
Cecilie Dinsen <ceci821s@stud.kea.dk>
2022-12-19 18:26:36 +0100

    cookie tilføjet

--- src/main.js
+++ src/main.js
@@ -39,3 +39,19 @@ wrappers.forEach((wrapper, index) => {
     elements[index].classList.toggle("article_info");
   });
 });
+
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+  cookieboks.classList.add("hidden");
+  sessionStorage.setItem("fjerncookieboks", true);
+}
+
+if (sessionStorage.getItem("fjerncookieboks")) {
+  fjernCookieBoks.classList.add("hidden");
+}
